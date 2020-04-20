af8dba65162c7388e645008a00625601a6329267
Sikander <sikander@lhote.me>
2020-04-20 15:07:06 +0800

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-# vim
-ln -s ${PWD}/.vimrc ~/
-
-# fish
-mkdir -p ~/.config/fish/
-ln -s ${PWD}/fish_variables ~/.config/fish/
-
-# bashrc
-ln -s ${PWD}/.bashrc ~/
+ansible-playbook playbook.yml --ask-vault-pass
