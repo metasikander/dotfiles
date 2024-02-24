ea63aa7e59e735fb4d11a8a4161644667b2787d5
sikander <sikander@debian.2s.localdomain>
2024-02-24 17:26:55 +0100

    cleanup

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,7 +7,7 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'AdventureTime'
+config.color_scheme = 'Catppuccin Macchiato'
 
 -- and finally, return the configuration to wezterm
 return config
