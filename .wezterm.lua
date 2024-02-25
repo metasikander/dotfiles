ab7dd9bcebcb7ade39fe38f2a94040e1ce3a7d84
sikander <sikander@debian.2s.localdomain>
2024-02-25 08:14:23 +0100

    wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,6 +6,7 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 config.hide_tab_bar_if_only_one_tab = false
+config.use_fancy_tab_bar = false
 
 config.color_scheme = 'Catppuccin Macchiato'
 
