f62cd5941201bd4ef54be38e361c419bae06a082
sikander <sikander@debian.2s.localdomain>
2024-02-25 08:10:29 +0100

    catppuccin and wezterm mods

--- .wezterm.lua
+++ .wezterm.lua
@@ -5,9 +5,13 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
+config.hide_tab_bar_if_only_one_tab = false
 
--- For example, changing the color scheme:
 config.color_scheme = 'Catppuccin Macchiato'
+
+config.font = wezterm.font 'Hack Nerd Font Mono'
+
+config.font_size = 14.0
 
 -- and finally, return the configuration to wezterm
 return config
