2e231ced925a7068e32eeb56ee4df117690132af
Sikander Lhote <sikander@x1.localdomain>
2024-08-29 21:16:50 +0200

    Update .wezterm.lua

--- empty_dot_wezterm.lua
+++ empty_dot_wezterm.lua
@@ -8,4 +8,7 @@ config.color_scheme = "Catppuccin Macchiato"
 -- Keep tabs visible, remove the title bar
 config.window_decorations = "RESIZE"
 
+-- Start in full size (fullscreen mode)
+config.initial_window_size = "fullscreen"
+
 return config
