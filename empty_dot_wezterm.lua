09e9a289ad81fcdd18ee81c4f5f22652f37cc554
Sikander Lhote <sikander@x1.localdomain>
2024-08-29 21:15:44 +0200

    Update .wezterm.lua

--- empty_dot_wezterm.lua
+++ empty_dot_wezterm.lua
@@ -5,4 +5,7 @@ local config = wezterm.config_builder()
 config.font = wezterm.font("Hack Nerd Font Mono")
 config.color_scheme = "Catppuccin Macchiato"
 
+-- Keep tabs visible, remove the title bar
+config.window_decorations = "RESIZE"
+
 return config
