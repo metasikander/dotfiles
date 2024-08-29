4a62f2213f41d7a5abe7911aa0b950a144a000a0
Sikander Lhote <sikander@x1.localdomain>
2024-08-29 21:12:11 +0200

    Update .wezterm.lua

--- empty_dot_wezterm.lua
+++ empty_dot_wezterm.lua
@@ -3,6 +3,6 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.font = wezterm.font("Hack Nerd Font Mono")
-config.color_scheme = "hardhacker"
+config.color_scheme = "Catppuccin Macchiato"
 
 return config
